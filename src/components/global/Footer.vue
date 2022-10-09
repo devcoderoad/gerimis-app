dfffc4d750dac97ab656c40d03fc664f3ddbde40
devcoderoad <devdef20@gmail.com>
2022-10-09 15:54:28 +0700

    chore: add link

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -17,6 +17,16 @@
           >Phosphor Icons
         </a>
       </div>
+      <div class="text-sm">
+        Read about
+        <a
+          href="https://medium.com/javascript-in-plain-english/how-to-build-vuejs-geo-location-weather-application-f65f5cc2fa4f"
+          target="_blank"
+          class="font-bold underline hover:no-underline"
+          title="Vue.js Geo-location Weather Application on Medium"
+          >this</a
+        >
+      </div>
     </div>
   </footer>
 </template>
